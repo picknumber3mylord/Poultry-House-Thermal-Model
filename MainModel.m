b280ea835783edc7bb04b98241067b9e7e465309
picknumber3mylord <45300611+picknumber3mylord@users.noreply.github.com>
2021-03-01 13:32:03 -0800

    Update MainModel.m

--- MainModel.m
+++ MainModel.m
@@ -1,6 +1,6 @@
 % Main Model for NZE Poultry House
 % Nathan Shang, Roxy Wilcox, Fermin Banuelos-Gonzalez
-% Edited 2/23/2021
+% Edited 3/1/2021
 
 clc
 clear all
@@ -15,16 +15,34 @@ clear all
     % poultry house geometry    
 
 % input reading
-inpData = xlsx
+inpData = readtable('Thermal_Model_Input_Data.xlsx')
 % organize input data into matrix
+
+tData = inpData(%indexing)  % vector of time values
+tempData = inpData(%indexing)   % matrix of hourly temperature data from NSRDB
+                                % each row corresponds to year
+radData = inpData(%indexing)    % solar radiation data from NSRDB
 
 % define constants necessary for thermal model
 
-Cp = %specific heat of air (J/kg/K)
-rho = %density of air (kg/m^3)
-ventRate = %ventilation rate (m^3/s)
-Tset = %inside temperature (C)
-Tout = %outside temperature (C)
+CpAir = 1000 (J/kg/K);
+rhoAir = 1.225 (kg/m^3);
+ventRate = inpData(%indexing);  % ventilation rate (m^3/s)
+
+% Don't think this will be needed
+% main loop will give inputs from tempData 
+% Tset = % inside temperature (C)
+% Tout = % outside temperature (C)
+
+% looping through NSRDB data points
+energyCost = 0;
+for i = 1:length(tData)
+    % do calcs
+    energyCost += ventE(CpAir, rhoAir, ventRate, Tset, tempData(%indexing))
+    energyCost += everything else
+end
+
+
 
 % function for finding heat loss for ventilation
 % inputs: specific heat of air, density of air
