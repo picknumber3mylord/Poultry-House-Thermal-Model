1a75d88eb5fced796a80a3b4bd4a644647ac7bf3
rhwilcox <79549485+rhwilcox@users.noreply.github.com>
2021-04-19 10:55:26 -0700

    Update MainModel.m

--- MainModel.m
+++ MainModel.m
@@ -49,9 +49,10 @@ SZA2018 = table2array(inpData2018(3:end,14))';
 SZA2017 = table2array(inpData2017(3:end,14))';
 SZA2016 = table2array(inpData2016(3:end,14))';
 
+% extract day numbers
+day = table2array(inpData2019(3:end,3);
+
 % define constants necessary for thermal model
-
-
 rSideInsu = designData(3,1);  %R-value of wall insulation (m^2*K/W)
 sideMetalCond = designData(1,1);  %thermal conductivity of metal siding material (W/m/K)
 sideMetalThick = designData(5,3);  %thickness of metal siding (m)
@@ -81,6 +82,12 @@ numChicken = 69376; %Number of chickens in the house
 lightOn = 5; %time in hours that house lights are turned on
 lightOff = lightOn + 15; %time in hours that house lights are turned off
 
+ventTilt = 90; %tilt of vents with respect to horizontal in degrees
+latitude = 38.5449; %latitude of Davis, CA in decimal degrees
+GHI = [GHI2016; GHI2017; GHI2018; GHI2019];
+SHGCvent = 0.13; %Solar Heat Gain Coefficient of Louver vents
+aVent = designData(5,7); %area of vents (m^2)
+
 %Energy Consumers%
 eggPackerE = 2.5; %Energy consumped by egg packer (kW*hr/day)
 eggBeltE = 237.44; %Energy consumped by egg belt (kW*hr/day)
@@ -93,7 +100,7 @@ manureBeltE = 20.768; %Energy consumped by manure belt (kW*hr/day)
 %constant, no matter the time
 rSideMetal = sideMetalThick/sideMetalCond;
 uSide = 1/(rSideInsu+rSideMetal);
-ventRate = 5*numChicken/3600;  % ventilation rate (m^3/s)
+ventRate = 12*numChicken/3600;  % ventilation rate (m^3/s)
 machinaryE = (eggPackerE + eggBeltE + eggElevatorE + eggWasherE + lightsE + manureBeltE) * 1000; %Sums all machinary energy costs and converts them into W*hrs/day
 
 fFloor = fFloor*1055.06*3.28084/3600; %Coverts F value of floor from BTU/hr/ft/F to W/m/F
@@ -111,13 +118,13 @@ tData = [0:length(tempData2019)-1];
 
 for i = 2:length(tData)
     % do calcs
-    energyCost2016(i) = wallE(uSide, aSide, Tset, Tout(1,i)) + ventE(CpAir, rhoAir, ventRate, Tset, Tout(1,i)) + floorE(pFloor, fFloor, Tset, Tout(1,i)) + roofE(uRoof, aRoof, Tset, Tout(1,i)) + chickE(tData(i), sensiDay, sensiNight, chickWeight, lightOn, lightOff, numChicken);
+    energyCost2016(i) = wallE(uSide, aSide, Tset, Tout(1,i)) + ventE(CpAir, rhoAir, ventRate, Tset, Tout(1,i)) + floorE(pFloor, fFloor, Tset, Tout(1,i)) + roofE(uRoof, aRoof, Tset, Tout(1,i)) + chickE(tData(i), sensiDay, sensiNight, chickWeight, lightOn, lightOff, numChicken) + solarE(SHGCvent, ventTilt, latitude, day(i), GHI(1,i), aVent);
                     
-    energyCost2017(i) = wallE(uSide, aSide, Tset, Tout(2,i)) + ventE(CpAir, rhoAir, ventRate, Tset, Tout(2,i)) + floorE(pFloor, fFloor, Tset, Tout(2,i)) + roofE(uRoof, aRoof, Tset, Tout(2,i)) + chickE(tData(i), sensiDay, sensiNight, chickWeight, lightOn, lightOff, numChicken);
+    energyCost2017(i) = wallE(uSide, aSide, Tset, Tout(2,i)) + ventE(CpAir, rhoAir, ventRate, Tset, Tout(2,i)) + floorE(pFloor, fFloor, Tset, Tout(2,i)) + roofE(uRoof, aRoof, Tset, Tout(2,i)) + chickE(tData(i), sensiDay, sensiNight, chickWeight, lightOn, lightOff, numChicken) + solarE(SHGCvent, ventTilt, latitude, day(i), GHI(2,i), aVent);
                     
-    energyCost2018(i) = wallE(uSide, aSide, Tset, Tout(3,i)) + ventE(CpAir, rhoAir, ventRate, Tset, Tout(3,i)) + floorE(pFloor, fFloor, Tset, Tout(3,i)) + roofE(uRoof, aRoof, Tset, Tout(3,i)) + chickE(tData(i), sensiDay, sensiNight, chickWeight, lightOn, lightOff, numChicken);
+    energyCost2018(i) = wallE(uSide, aSide, Tset, Tout(3,i)) + ventE(CpAir, rhoAir, ventRate, Tset, Tout(3,i)) + floorE(pFloor, fFloor, Tset, Tout(3,i)) + roofE(uRoof, aRoof, Tset, Tout(3,i)) + chickE(tData(i), sensiDay, sensiNight, chickWeight, lightOn, lightOff, numChicken) + solarE(SHGCvent, ventTilt, latitude, day(i), GHI(3,i), aVent);
                     
-    energyCost2019(i) = wallE(uSide, aSide, Tset, Tout(4,i)) + ventE(CpAir, rhoAir, ventRate, Tset, Tout(4,i)) + floorE(pFloor, fFloor, Tset, Tout(4,i)) + roofE(uRoof, aRoof, Tset, Tout(4,i)) + chickE(tData(i), sensiDay, sensiNight, chickWeight, lightOn, lightOff, numChicken);
+    energyCost2019(i) = wallE(uSide, aSide, Tset, Tout(4,i)) + ventE(CpAir, rhoAir, ventRate, Tset, Tout(4,i)) + floorE(pFloor, fFloor, Tset, Tout(4,i)) + roofE(uRoof, aRoof, Tset, Tout(4,i)) + chickE(tData(i), sensiDay, sensiNight, chickWeight, lightOn, lightOff, numChicken) + solarE(SHGCvent, ventTilt, latitude, day(i), GHI(4,i), aVent);
                     
 end
 
@@ -168,6 +175,23 @@ function chickEnergy = chickE(t, sensiDay, sensiNight, chickWeight, lightOn, lig
     else 
         chickEnergy = numChicken*sensiNight*chickWeight; %during dark hours, use sensible heat production for night
     end 
+end
+
+%function for calculating solar radiation energy gain 
+%inputs: SHGC, tilt of surface, latitude, day of year, GHI, area of surface
+%outputs: Energy gain in Watts
+function solarEnergy = solarE(SHGC, tilt, latitude, day, GHI, area)
+    solarR = solarRad(tilt, latitude, day, GHI); %total solar radiation in W/m^2
+    solarEnergy = SGHC*solarR*area; %energy gain from solar radiation in W
+end
+
+%function for calculating solar radiation incident on surface
+%inputs: tilt angle in degrees, latitude, day of year, GHI
+%outputs: Solar radiation incident on surface in W/m^2
+function solarR = solarRad(tilt, latitude, day, GHI)
+    delta = 23.45*sind((360/365)*(284+day));
+    alpha = 90 - latitude + delta;
+    solarR = (GHI*sind(alpha+tilt))/sind(alpha);
 end
 
 %function to convert from deg Celcius to deg Fahrenheit
